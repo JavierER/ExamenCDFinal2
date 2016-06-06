def2e57c470ffc53eccc8ba184b507dac9be44f1
Javii <Javii@Javi>
2016-06-06 09:30:11 +0200

    Renames añadido para mejor comprension de las variables (Refactor-->Rename)

--- src/examenfinalparte2/Principal.java
+++ src/examenfinalparte2/Principal.java
@@ -5,47 +5,47 @@ public class Principal {
     public static boolean p = false;
 
     public static void main(String arg[]) {
-        int dig = 2;
-        int ndig = 0;
-        if (dig <= 0) {
+        int Digito = 2;
+        int cargaDigitos = 0;
+        if (Digito <= 0) {
             System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
         }
         for (int i = 1; i <= 99999; i++) {
             int aux = i;
 
-            int contador = 0;
+            int contador1 = 0;
 
             while (aux != 0) {
                 aux = aux / 10;
-                contador++;
+                contador1++;
             }
-            ndig = contador;
+            cargaDigitos = contador1;
 
-            if (ndig == dig) {
+            if (cargaDigitos == Digito) {
                 if (i < 4) {
                     p = true;
                 } else {
                     if (i % 2 == 0) {
                         p = false;
                     } else {
-                        int contador1 = 0;
-                        int i1 = 1;
+                        int contador2 = 0;
+                        int Digito1 = 1;
                         int k = (i - 1) / 2;
                         if (k % 2 == 0) {
                             k--;
                         }
 
-                        while (i1 <= k) {
-                            if (i % i1 == 0) {
-                                contador1++;
+                        while (Digito1 <= k) {
+                            if (i % Digito1 == 0) {
+                                contador2++;
                             }
-                            i1 += 2;
-                            if (contador1 == 2) {
-                                i1 = k + 1;
+                            Digito1 += 2;
+                            if (contador2 == 2) {
+                                Digito1 = k + 1;
                             }
                         }
 
-                        if (contador1 == 1) {
+                        if (contador2 == 1) {
                             p = true;
                         }
                     }
